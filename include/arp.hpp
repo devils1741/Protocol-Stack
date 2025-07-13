faa30c960c0408b74ca60b5d64922555dbefc7ad
devils1741 <devils1741@gmail.com>
2025-07-13 19:59:51 +0800

    add struct  arp

--- include/arp.hpp
+++ include/arp.hpp
@@ -6,13 +6,20 @@
 
 using std::list;
 
-struct arp
+/**
+ * @brief arp的数据结构
+ */
+struct arp_header
 {
-    /* data */
+    uint16_t hardware_type;      // 硬件类型
+    uint16_t protocol_type;      // 协议类型
+    uint8_t hardware_length;     // 硬件地址长度
+    uint8_t protocol_length;     // 协议地址长度
+    uint16_t operation;          // 操作码
+    uint8_t sender_hwaddr[6];    // 发送方硬件地址
+    uint8_t sender_protoaddr[4]; // 发送方协议地址
+    uint8_t target_hwaddr[6];    // 目标硬件地址
+    uint8_t target_protoaddr[4]; // 目标协议地址
 };
 
-
-
-
 #endif
-
